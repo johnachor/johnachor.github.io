8a53f217ba57af85f6eaa565ec5b32de7b483df1
John Achor <johnachor@gmail.com>
2018-03-20 17:23:35 -0500

    blog 2 complete

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -1,10 +1,22 @@
 var blogentries = [
   {
-    id: "blog1",
+    id: "blog01",
     title: "First couple weeks back!",
     date: "March 12, 2018",
     content: "It's good to be back in the saddle!  A crash course through the Treehouse material has me raring to go and feeling freshly smart -- even if I feel like I have to re-teach myself how CSS flex works every time I make a new page.  Looking forward to making a bunch of new friends and learning React instead of Angular this time around.  Here's to a new start!"
-  }
+  },
+  {
+    id: "blog02",
+    title: "Relearning Vanilla Javascript",
+    date: "March 14, 2018",
+    content: "Now that prework is completely out of the way and we have study groups and Javascript in our toolkit, I'm feeling much more at home again.  I am honestly addicted to just making JS do things with data.  This time around I want to go out of my way to work in with my teammates and write code that they can understand and that fits in with the overall project, rather than expecting everyone to adopt my philosophies."
+  },
+  {
+    id: "blog03",
+    title: "Teamwork and Git",
+    date: "March 20, 2018",
+    content: ""
+  }  
 ];
 
 function createBlogCard(blogObject) {
