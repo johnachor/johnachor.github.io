f3301acbe7d2f991cfa628e84b70fd722dc41f4a
John Achor <johnachor@gmail.com>
2018-06-23 16:46:44 -0500

    unignored compiled stuff for hosting, fixed nav and foot colors

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@ $('#main-carousel').flickity({
   setGallerySize: false,
   pageDots: false,
   wrapAround: true,
+  initialIndex: 2,
 });
 
 $('.flickity-prev-next-button').addClass('hide-on-med-and-down');
