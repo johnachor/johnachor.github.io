94d8631322951d7e95e9694849e3503b1ed14d29
John Achor <johnachor@gmail.com>
2018-06-24 17:09:37 -0500

    now retrieving projects from firebase and displaying

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -3,11 +3,20 @@ const baseUrl = 'https://johnachor-github-personal.firebaseio.com/';
 
 const getContent = () => {
   getBlogs().then(data => $('#blog-entries').html(data.reverse().map(stringbuilder.buildBlogCard)));
+  getProjects().then(data => $('#projects-entries').html(data.map(stringbuilder.buildProjectCard)));
 };
 
 const getBlogs = () => {
   return new Promise((resolve, reject) => {
     $.get(`${baseUrl}/blogentries.json`)
+      .fail(err => reject(err))
+      .done(data => resolve(data));
+  });
+};
+
+const getProjects = () => {
+  return new Promise((resolve, reject) => {
+    $.get(`${baseUrl}/projects.json`)
       .fail(err => reject(err))
       .done(data => resolve(data));
   });
--- javascripts/stringbuilder.js
+++ javascripts/stringbuilder.js
@@ -2,11 +2,38 @@ const buildBlogCard = (blogEntry) => {
   return `
   <div class="blog-card card">
     <div class="card-content">
-      <span class="card-title">${blogEntry.title}</span>
-      <p>${blogEntry.content}</p>
+      <span class="card-title">${blogEntry.title.toLowerCase()}</span>
+      <p>${blogEntry.content.toLowerCase()}</p>
     </div>
     <div class="card-action">
-      <p>${blogEntry.date}</p>
+      <p>${blogEntry.date.toLowerCase()}</p>
+    </div>
+  </div>
+  `;
+};
+
+const buildProjectCard = (project) => {
+  return `
+  <div class="col s12 m6 l4">
+    <div class="card project-card sticky-action">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="${project.image}">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator">${project.title.toLowerCase()}
+          <i class="material-icons right">more_vert</i>
+        </span>
+        <div class="card-action">
+          <p><a href="${project.repo}">check it out on github</a></p>
+          ${project.deployed ? '<p><a href="' + project.deployed + '">see the deployed site</a></p>' : ''}
+        </div>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${project.title.toLowerCase()}
+          <i class="material-icons right">close</i>
+        </span>
+        <p class="black-text">${project.description.toLowerCase()}</p>
+      </div>
     </div>
   </div>
   `;
@@ -14,4 +41,5 @@ const buildBlogCard = (blogEntry) => {
 
 module.exports = {
   buildBlogCard,
+  buildProjectCard,
 };
